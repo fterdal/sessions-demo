46551c24573580a4c7f5af576548c417eecb47c8
Finn <finnterdal@gmail.com>
2020-07-23 00:23:37 -0500

    slight adjustment to starting

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ app.use(morgan('dev'));
 app.use(
   session({
     // this mandatory configuration ensures that session IDs are not predictable
-    secret: '2006-FSA-RM-CHI is cool', // or whatever you like
+    secret: 'i want a smoothie so bad rn', // or whatever you like
     // this option says if you haven't changed anything, don't resave. It is recommended and reduces session concurrency issues
     resave: false,
     // this option says if I am new but not modified still save
@@ -21,7 +21,9 @@ app.get('/', (req, res, next) => {
   console.log('Session ID:', req.session.id);
   console.log('Headers: ', req.headers);
   console.log('Cookie: ', req.headers.cookie);
-  res.send('Hello');
+  res.send(`
+  <h1>Hello</h1>
+  `);
 });
 const PORT = 8080;
 
